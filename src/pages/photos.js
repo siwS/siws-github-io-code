9013b24c78f5222ff9912d0ca06060f48827e6e3
sofia <tzi.sof@gmail.com>
2019-05-19 22:31:52 +0100

    Move menu to layout component

---
+++ src/pages/photos.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <h2>Photos</h2>
+
+  </Layout>
+)
+
+export default SecondPage
