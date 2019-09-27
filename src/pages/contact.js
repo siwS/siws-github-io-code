8d65cd205558cea03240249708c27f8999cdab0e
sofia <tzi.sof@gmail.com>
2019-09-27 23:26:09 +0100

    Deploy

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,14 +2,29 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IntercomApp from "../components/intercom-app.js"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Contact" />
-    <h2>Contact</h2>
+const ContactPage = () => (
+  <div>
+    <Layout>
+      <SEO title="Contact" />
+      <h2>Contact</h2>
+      <p>
+        Hey! If you wanna chat about work, or grab a coffee, you can reach out:
+      </p>
+
+      <ul>
+        <li>email: <a href="mail-to:tzi.sof@gmail.com">tzi.sof-at-gmail.com</a></li>
+        <li>twitter: <a href="https://twitter.com/SophieSiw">SophieSiw</a></li>
+        <li>LinkedIn: <a href="https://www.linkedin.com/in/sofiatzima/">Sofia Tzima</a></li>
+        <li>or you can write to me in Intercom</li>
+      </ul>
 
 
-  </Layout>
+
+    </Layout>
+    <IntercomApp/>
+  </div>
 )
 
-export default SecondPage
+export default ContactPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,8 +28,6 @@ const IndexPage = () => (
 
   </Layout>
 
-    <IntercomApp/>
-
   </div>
 )
 
